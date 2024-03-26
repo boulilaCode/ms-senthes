6c94c360a286ab07f7acb22c2334c50f7ced4832
boulilaCode <fatiboulila1991@gmail.com>
2024-03-26 22:49:51 +0000

    my firs commit

---
+++ invintory-service/src/main/java/org/hicham/invintoryservice/InvintoryServiceApplication.java
@@ -0,0 +1,13 @@
+package org.hicham.invintoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvintoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvintoryServiceApplication.class, args);
+	}
+
+}
---
+++ invintory-service/src/test/java/org/hicham/invintoryservice/InvintoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hicham.invintoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvintoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
